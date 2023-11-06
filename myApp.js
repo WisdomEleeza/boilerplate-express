488e4b905f3077ba0a02f7b2d758cef523d91ade
Wisdom Eleeza <whiy07@gmail.com>
2023-11-06 19:58:50 +0000

    add get request to send a json data

--- myApp.js
+++ myApp.js
@@ -3,9 +3,10 @@ let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
+app.get('/json', (req, res) => {
     const absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath)
+    res.json({ message: "Hello json" })
 });
 
 
