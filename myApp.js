1ef5d2f247615a66924d79a11f3406e754bcacde
Wisdom Eleeza <whiy07@gmail.com>
2023-11-06 20:11:49 +0000

    add env file to the project file

--- myApp.js
+++ myApp.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
+
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/json', (req, res) => {
     const absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath)
-    res.json({ message: "Hello json" })
+    res.json({ message: process.env.MESSAGE_STYLE })
 });
 
 
