f23638350113324c44d06a9deeb86d25c6b4372b
Wisdom Eleeza <whiy07@gmail.com>
2023-11-06 19:11:18 +0000

    add get request for sending static path using express middleware

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
 let express = require('express');
 let app = express();
 
+app.use('/public', express.static())
+
 app.get('/', (req, res) => {
-    const absolutePath = __dirname + '/views/index.html'
+    // const absolutePath = __dirname + '/views/index.html'
+    const absolutePath = __dirname + '/public'
     res.sendFile(absolutePath)
 });
 
