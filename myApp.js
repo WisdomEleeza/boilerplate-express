32d8a267c7ed82f5d3d7894bba54d5de0a6e3ca8
Wisdom Eleeza <whiy07@gmail.com>
2023-11-07 10:47:34 +0000

    add env file to the project file(9)

--- myApp.js
+++ myApp.js
@@ -6,14 +6,15 @@ let app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/json', (req, res) => {
-    // const absolutePath = __dirname + '/views/index.html';
-    // res.sendFile(absolutePath);
-  
     if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.json({ message: "Hello json" });
-    } else {
         res.json({ message: "HELLO JSON" });
+    } else {
+        res.json({ message: "Hello json" });
     }
+
+    // Send the HTML file after sending the JSON response
+    const absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
 });
 
 
